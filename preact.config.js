4df8e9f96d95bbd0d81214d9c354426b25761bfb
James Scott-Brown <james@jamesscottbrown.com>
2020-11-06 15:28:43 +0000

    changes to config for github pages deployment to work

--- preact.config.js
+++ preact.config.js
@@ -1,9 +1,9 @@
 // See https://stackoverflow.com/questions/45742982/set-base-url-for-preact-cli
 
 // You may need to comment out this block during development
-/*
+// /*
 export default (config, env, helpers) => {
-  config.output.publicPath = "/risk/4c/";
+  config.output.publicPath = "/deterioration-calculator/";
 
   // use the public path in your app as 'process.env.PUBLIC_PATH'
   config.plugins.push(
@@ -14,4 +14,4 @@ export default (config, env, helpers) => {
     })
   );
 };
-  */
+//  */
