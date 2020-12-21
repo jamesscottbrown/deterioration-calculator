83409617abd74916e6a83ca354fcf78c22c7d589
James Scott-Brown <james@jamesscottbrown.com>
2020-12-21 10:44:43 +0000

    tweaks to intro text suggested by Callum

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,7 @@ export const Intro = () => (
       tools that predict in-hospital mortality or in-hospital clinical
       deterioration (defined as any requirement of ventilatory support or
       critical care, or death) for hospitalised COVID-19 patients, produced by
-      the <a href="https://isaric4c.net/">ISARIC 4C consortium</a>.{" "}
+      the <a href="https://isaric4c.net/">ISARIC4C consortium</a>.{" "}
       <b>They are intended for use by clinicians.</b>
     </p>
     <p>
@@ -13,23 +13,23 @@ export const Intro = () => (
       at hospital presentation.
     </p>
     <p>
-      They are based on a UK cohort of patients, and should not be adopted for
-      routine clinical use in other settings until it has been appropriately
-      validated.
+      They are based on a large UK cohort of patients, and should not be adopted
+      for routine clinical use in other settings until they have been
+      appropriately validated for these settings.
     </p>
     <p>
       For full details, see
       <a href="https://doi.org/10.1136/bmj.m3339"> the paper</a> introducing the
-      mortality score, and the{" "}
+      4C Mortality Score, and the{" "}
       <a href="https://www.medrxiv.org/content/10.1101/2020.10.09.20209957v1">
         preprint
       </a>{" "}
-      introducing the deterioration model.
+      introducing the 4C Deterioration model.
     </p>
     <p>
       This is an infographic that visualises risk, based on observed mortality
-      and deterioration among hospitalised adult COVID19 patients recruited into
-      the ISARIC 4C study in the UK.
+      and deterioration among hospitalised adult COVID19 patients enrolled to
+      the ISARIC WHO Clinical Characterisation Protocol UK study.
     </p>
   </>
 );
