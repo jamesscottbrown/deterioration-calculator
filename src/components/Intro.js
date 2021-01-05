c43d5fcb7b19ec8cc209713d120aad95f8f018a4
James Scott-Brown <james@jamesscottbrown.com>
2021-01-05 21:53:46 +0000

    tweak message about clinical use and update paper link

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,8 +6,17 @@ export const Intro = () => (
       deterioration (defined as any requirement of ventilatory support or
       critical care, or death) for hospitalised COVID-19 patients, produced by
       the <a href="https://isaric4c.net/">ISARIC4C consortium</a>.{" "}
-      <b>They are intended for use by clinicians.</b>
     </p>
+    <p>
+      The Scores are intended to be considered only by clinicians to aid their
+      clinical decision-making, however clinicians should ultimately rely on
+      their own clinical judgement in treatment decisions. In compliance with
+      the Medical Devices Regulations 2002, the raw data generating the Scores
+      and look up table to estimate mortality and probability of deterioration
+      are provided for the clinician to review and as such this webpage is not
+      considered to be a medical device.
+    </p>
+
     <p>
       They are designed to require only parameters that are commonly available
       at hospital presentation.
@@ -26,9 +35,7 @@ export const Intro = () => (
       For full details, see
       <a href="https://doi.org/10.1136/bmj.m3339"> the paper</a> introducing the
       4C Mortality Score, and the{" "}
-      <a href="https://www.medrxiv.org/content/10.1101/2020.10.09.20209957v1">
-        preprint
-      </a>{" "}
+      <a href="https://doi.org/10.1016/S2213-2600(20)30559-2">paper</a>{" "}
       introducing the 4C Deterioration model.
     </p>
     <p>
